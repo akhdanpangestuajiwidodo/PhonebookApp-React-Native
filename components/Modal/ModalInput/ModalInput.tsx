8968f9b90ddf8301dc4a8dae72ca95ffc6941d7a
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-07 14:20:34 +0700

    initial function get data

--- components/Modal/ModalInput/ModalInput.tsx
+++ components/Modal/ModalInput/ModalInput.tsx
@@ -10,6 +10,7 @@ import {
   Alert,
   TextInput,
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface ModalState {
   modalVisible: boolean;
@@ -21,13 +22,48 @@ let sub: {[key: string]: any} = {
   phoneNumber: null,
   avatarImage: null,
 };
+
+type formDataItem = {
+  name: string;
+  phoneNumber: string;
+  avatarImage: string;
+};
 const ModalInput = (props: ModalState) => {
+  const result: formDataItem[] = [];
+  //Hooks Contacts
+  const [contacts, setContact] = useState(result);
+  //Hooks form data
   const [formData, setFormData] = useState(sub);
+
+  //Hooks test
+  const [testText, setTestText] = useState(1);
+
+  //Handle change value form input
   function handleChange(event: any) {
     let data = {...formData};
     data[event.target.name] = event.target.value;
     setFormData(data);
   }
+
+  //Save Data to local
+  const storeData = async () => {
+    try {
+      let data = [...contacts];
+      data.push({
+        name: formData.name,
+        phoneNumber: formData.phoneNumber,
+        avatarImage: '../../image/user.jpg',
+      });
+      setContact(data);
+      setFormData({name: '', phoneNumber: ''});
+      const jsonValue = JSON.stringify(data);
+      await AsyncStorage.setItem('contacts', jsonValue);
+      props.setModalVisible(!props.modalVisible);
+    } catch (e) {
+      // saving error
+    }
+  };
+
   return (
     <Modal
       animationType="slide"
@@ -39,7 +75,7 @@ const ModalInput = (props: ModalState) => {
       }}>
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
-          <Text style={styles.modalText}>Input Data Contact</Text>
+          <Text style={styles.modalText}>Input Data Contact {testText}</Text>
           <View>
             <TextInput
               style={styles.searchInput}
@@ -58,7 +94,9 @@ const ModalInput = (props: ModalState) => {
               placeholder={'08xxxxx'}
             />
           </View>
-          <Pressable style={[styles.button, styles.buttonSave]}>
+          <Pressable
+            style={[styles.button, styles.buttonSave]}
+            onPress={() => storeData}>
             <Text style={styles.textStyle}>Save</Text>
           </Pressable>
           <Pressable
